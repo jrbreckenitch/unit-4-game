2e2cb7c36c738afbf55ba022961abf11d0b6bc55
jrbreckenitch <jamesbreckenitch@Jamess-MacBook-Pro.local>
2018-07-27 16:49:41 -0500

    added HTML, CSS, and PseudoCode

---
+++ assets/javascript/game.js
@@ -0,0 +1,13 @@
+// generate random number at start of each game between 19 - 120
+
+// display random number to page at start of each game
+
+// each crystal has hidden random value between 1 - 12 (generates new random value each time game begins)
+
+// crystal will generate random value each time it is clicked
+
+// game restarts whenever player wins or loses and updates win/losses
+
+// generated new random number when game restarts
+
+// do not refresh page to restart the game
